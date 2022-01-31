9c9b3d4cd9e2c544cf4ac8f645501c57579a43c2
3600-abhi <abhishekjaiswal3600@gmail.com>
2022-01-31 21:37:36 +0530

    table with sticky headers created successfully

--- lib/screens/TabularRepresentation.dart
+++ lib/screens/TabularRepresentation.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:real_time_weather_update/weatherWithTime.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:data_table_2/data_table_2.dart';
 
 class TabularRepresentation extends StatefulWidget {
   DateTime? pickedDate = null;
@@ -31,7 +32,10 @@ class _TabularRepresentationState extends State<TabularRepresentation> {
   _TabularRepresentationState(
       this.pickedDate, this.startingTime, this.endingTime);
 
-  List<weatherWithTime> weatherTimeList = [];
+  static List<weatherWithTime> weatherTimeList = [];
+
+  // data for tables
+  static List<String> columnTable = ['Time', 'Temperature', 'Humidity'];
 
   Future<List<weatherWithTime>> fetchData() async {
     // formatting the starting date and time
@@ -92,7 +96,7 @@ class _TabularRepresentationState extends State<TabularRepresentation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Graphical Representation')),
+      appBar: AppBar(title: Text('Tabular Representation')),
       body: Center(
         child: FutureBuilder(
           future: fetchData(),
@@ -102,51 +106,29 @@ class _TabularRepresentationState extends State<TabularRepresentation> {
                 child: Text('Table is loading please wait...'),
               );
             } else {
-              return SingleChildScrollView(
-                  child: Table(
-                columnWidths: {
-                  0: FractionColumnWidth(0.5),
-                  1: FractionColumnWidth(0.3),
-                  2: FractionColumnWidth(0.2)
-                },
-                border: TableBorder.all(
-                    color: Colors.white, style: BorderStyle.solid),
-                children: weatherTimeList
-                    .map((e) => TableRow(
-                          children: [
-                            TableCell(
-                                child: IntrinsicHeight(
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(e.time,
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                      )),
-                                  VerticalDivider(
-                                      color: Colors.white, thickness: 2),
-                                  Text(e.temperature,
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                      )),
-                                  VerticalDivider(
-                                      color: Colors.white, thickness: 2),
-                                  Text(e.humidity,
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                      ))
-                                ],
-                              ),
-                            ))
-                          ],
-                        ))
-                    .toList(),
-              ));
+              return DataTable2(
+                  columns: getColumns(),
+                  rows: getRows(),
+              );
             }
           },
         ),
       ),
     );
   }
+}
+
+List<DataColumn> getColumns() {
+  return _TabularRepresentationState.columnTable.map((e) => DataColumn(label: Text(e))).toList();
+}
+
+List<DataRow> getRows() {
+  return _TabularRepresentationState.weatherTimeList.map((e) {
+    List<String> cells = [e.time,e.temperature,e.humidity];
+    return DataRow(cells: getCells(cells));
+  }).toList();
+}
+
+List<DataCell> getCells(List<String> cells) {
+  return cells.map((e) => DataCell(Text(e))).toList();
 }
